c2fd2e1cbee8e684e1e131369998d5e14cff3019
mditbbte <andrii.ds.kor@gmail.com>
2019-12-09 15:03:09 +0200

    Updates

---
+++ precache-manifest.6d991b023c5e7c91ebaac5842bd1e624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d4eb3e04a08680f893f2298987dc88",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "7f18b1320c85b5cb04c9",
+    "url": "/goit-react-hw-04-movies/static/css/main.3353679a.chunk.css"
+  },
+  {
+    "revision": "6cf7f679d9c872f235b0",
+    "url": "/goit-react-hw-04-movies/static/js/2.64c6dcec.chunk.js"
+  },
+  {
+    "revision": "7f18b1320c85b5cb04c9",
+    "url": "/goit-react-hw-04-movies/static/js/main.a8b24d14.chunk.js"
+  },
+  {
+    "revision": "787dec3b397379bdb9f4",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.1957d60a.js"
+  }
+]);
